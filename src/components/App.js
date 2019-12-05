956fb75cf43e91f82210c092e4be42e46839728d
Tyler Cloetingh <tylercloetingh@Tylers-MacBook-Air.local>
2019-12-05 08:30:39 -0500

    working ajax

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,17 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  };
+  state = { images: [] };
+
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID e8003296245dd5b29009d5c12f58530d5801877044c99710f93bf317d604965c'
+      }
+    });
+    console.log(response.data.results);
+  }
   render() {
     return (
       <div className="ui container" style={{ margin: '10px'}}>
