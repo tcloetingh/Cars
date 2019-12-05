f167c588eeaade6b9f3014e13a9d6064b2fe240f
Tyler Cloetingh <tylercloetingh@Tylers-MacBook-Air.local>
2019-12-04 21:55:17 -0500

    this.state fix

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.term)
+  }
+  
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" />
+      <div onSubmit={this.onFormSubmit} className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" value={this.state.term} onChange={(e) => this.setState({term: e.target.value })} />
+          </div>
         </form>
       </div>
     );
